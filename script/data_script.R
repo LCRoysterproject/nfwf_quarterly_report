de0608584b7c995079d73aea51b5be05f6e66318
Mel Moreno <melimore86@ufl.edu>
2019-10-21 16:22:48 -0400

    updated to latest wq to make the reports, edited some formatting

--- script/data_script.R
+++ script/data_script.R
@@ -39,25 +39,63 @@ wq$observation_datetime<- as.Date(wq$observation_datetime, tz="EST",usetz=TRUE)
 
 colnames(wq) <- c("ID", "Obs_Date", "In_Service", "Pressure", "Temperature", "Conductivity", "Salinity_OG", "Sound_velo",  "Site","Sensor_ID", "Salinity","Date")
 
-# A quick ggplot test
-ggplot(data= wq,aes( x=Date, y= Salinity))+
-  geom_point()+
-  facet_wrap (~Site)
+write.csv(wq,file = "data/wq_total.csv")
 
-## Removing dates where sensors were malfunctioning in Site 2, oyster growth
-wq<- wq %>% 
-  filter(!(Site == 2 & Salinity < 0.5))
-
-## Removing dates where sensors were malfunctioning in Site 3
+## Removing any values over 40 ppt
 wq<-wq %>% 
-  filter(!(Site == 3 & Salinity < 4))
+  filter(!(Salinity > 40))
+
+## Removing dates where sensors were malfunctioning in Site 2, oyster growth from 10/31/2017- 12/1/2017
+wq<- wq %>% 
+  filter(!(Site == 2 > "2017-10-31 23:00:00" & Date < "2017-12-01 23:00:00" ))
+
+#Removing Site 10 values before a certain time of when the sensor was function
+wq<-wq %>% 
+  filter(!(Site == 10 & Date < "2019-03-08 15:03:00"))
+
+#Removing this values because of removal and redeployment of sensor
+wq<- wq %>% 
+  filter(!(Site == 2 & Date > "2019-04-27 19:00:00" & Date < "2019-05-06 14:00:00" ))
+
+#Removing values from site 6, sensor is faulty between July 16, 2019 to August 20, 2019, starting at 17:00 
+wq<- wq %>% 
+  filter(!(Site == 6 & Date > "2019-07-16 23:00:00" & Date < "2019-08-20 17:00:00" ))
+
+# Removing a flatline reading in the code 
+wq<-wq %>% 
+  filter(!(Site == 11 & Date > "2019-07-29 23:00:00" & Date < "2019-08-02 23:00:00"))
+
+# Removing a flatline reading in the code for site 9
+wq<-wq %>% 
+  filter(!(Site == 9 & Date > "2019-09-05 23:00:00" & Date < "2019-09-27 23:00:00"))
+
+#Removing observations from site 4, Flatiline in early September 
+wq<-wq %>% 
+  filter(!(Site == 4 & Date > "2019-08-28 23:00:00" & Date < "2019-09-10 23:00:00"))
+
+#Removing observations from site 2, Flatiline in early October 
+wq<-wq %>% 
+  filter(!(Site == 2 & Date > "2019-10-01 23:00:00" & Date < "2019-10-15 23:00:00"))
+
+#Removing all trial 
+wq<-wq %>% 
+  filter(!(Site == 0))
+
+#Annalees sensors
+wq<-wq %>% 
+  filter(!(Site == 21))
+wq<-wq %>% 
+  filter(!(Site ==22))
+wq<-wq %>% 
+  filter(!(Site ==22))
 
 
 #Writting as a .csv for the Shiny App
 write.csv(wq,file = "data/wq.csv")
 
 
-###Lakewatch Data, from the MySQL workbench
+
+###Lakewatch and YSI Data, from the MySQL workbench
 
 
 #Connecting to Database to update file 
@@ -75,6 +113,9 @@ lab$Secchi<- (lab$Secchi/ 3.28)
 # We need to update the sensor_type to the correct names for facetting, 4= YSI and 5= Lakewatch 
 lab$Sensor_Type[lab$Sensor_Type == "4"] <- "LAKEWATCH"
 lab$Sensor_Type[lab$Sensor_Type == "5"] <- "YSI"
+
+lab<-lab %>% 
+  filter(!(Site == 0))
 
 #Writting as a .csv for the Shiny App
 write.csv(lab, file = "data/lab.csv")
